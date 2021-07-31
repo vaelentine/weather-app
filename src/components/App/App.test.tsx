c50c94e802750a82c1f6dc3fa1292a536ef834ef
vaelentine <magdalenegeise@gmail.com>
2021-07-30 20:59:33 -0700

    add app tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('should render a search bar', () => {
+  it('should render a search container', () => {
     render(<App />);
-    const search_bar = screen.getByPlaceholderText(/search/i) //should contain the word "search"
-    expect(search_bar).toBeInTheDocument();
+    const search_container = document.querySelector('#SearchContainer')
+    expect(search_container).not.toBeNull();
   })
-  it('should render a search button labeled "Get Weather"', () => {
-    render(<App />);
-    const button = screen.getByText(/Get weather/i) 
-    expect(button).toBeInTheDocument();
+  it('should render a weather container', () => {
+    const weather_container = document.querySelector('#WeatherContainer')
+    expect(weather_container).not.toBeNull();
+  })
+  it('should render a footer', () => {
+    const footer_component = document.querySelector('#Footer')
+    expect(footer_component).not.toBeNull();
   })
 })
-
-
