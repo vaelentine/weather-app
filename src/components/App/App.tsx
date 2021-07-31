5629c0e9daab2739b4287499ef9c5798127440fe
vaelentine <magdalenegeise@gmail.com>
2021-07-30 20:58:46 -0700

    add app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Search from '../Search/Search';
+import WeatherView from '../WeatherView/WeatherView';
+import OpenWeatherHandler from '../../services/OpenWeatherHandler'
+import Footer from '../Footer/Footer';
 
 function App() {
+  const weatherObject = new OpenWeatherHandler();
+
   return (
     <div className="App">
+      <Search weatherObj={weatherObject}/>
+      <WeatherView weatherObj={weatherObject}/>
+      <Footer />
     </div>
   );
 }
