58831ec61da83ef204587b7b4412814e4a52eee7
vaelentine <magdalenegeise@gmail.com>
2021-08-03 06:33:24 -0700

    add api key error banner; update headers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,18 +3,28 @@ import Search from '../Search/Search';
 import WeatherView from '../WeatherView/WeatherView';
 import OpenWeatherHandler from '../../services/OpenWeatherHandler'
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const weatherObject = new OpenWeatherHandler();
+const missingApiKey: boolean = weatherObject.apiKey ===  undefined;
 
 function App() {
   const [ weatherState, setWeatherState  ] = useState('')
   weatherObject.assignState(setWeatherState);
   return (
-    <div className="App">
-      <Search weatherObj={weatherObject}/>
-      <WeatherView weatherObj={weatherObject} weatherState={weatherState}/>
-      <Footer />
+    <div className="appContainer">
+      { missingApiKey && <div className="missingKey"> <p>Warning: It looks like your API key isn't configured. </p>
+      <p>Review the read me file for instructions to set one up.</p></div>}
+      <div className="App">
+        <div className="Header">
+        What's the weather in ...
+        </div>
+        <Search weatherObj={weatherObject} weatherState={weatherState}/>
+        <WeatherView weatherObj={weatherObject} weatherState={weatherState}/>
+        <Footer />
+      </div>
     </div>
+
   );
 }
 
