0ac6c1da189913178deb181fdc9b3ba5c07c49f8
vaelentine <magdalenegeise@gmail.com>
2021-08-03 11:40:15 -0700

    update search tests

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -3,16 +3,18 @@ import { render, screen } from '@testing-library/react';
 import Search from './Search';
 import OpenWeatherHandler from '../../services/OpenWeatherHandler'
 
+
 describe('SearchForm', () => {
   const weatherObject = new OpenWeatherHandler();
   it('should render a search bar', () => {
     render(<Search weatherObj={weatherObject}/>);
-    const search_bar = screen.getByPlaceholderText(/search/i) //should contain the word "search"
+    const search_bar = screen.getByPlaceholderText(/city/i) //should contain the word "city"
     expect(search_bar).toBeInTheDocument();
   })
-  it('should render a search button labeled "Get Weather"', () => { 
+  it('should render a search button labeled "Find out"', () => { 
     render(<Search weatherObj={weatherObject}/>);
-    const button = screen.getByText(/Get weather/i) 
+    const button = screen.getByText(/Find out/i) 
     expect(button).toBeInTheDocument();
   })
+
 })
