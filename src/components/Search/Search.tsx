ef610e69bab7d98a2ba46e61b815644e5a1ddb84
vaelentine <magdalenegeise@gmail.com>
2021-08-03 06:34:38 -0700

    update error handling; add state

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import OpenWeatherHandler from '../../services/OpenWeatherHandler'
+import './Search.css'
 
 interface WeatherProps {
     weatherObj: OpenWeatherHandler;
+    weatherState: any;
 }
 
-const Search: React.FC<WeatherProps> = ({weatherObj}) => {
+const Search: React.FC<WeatherProps> = ({weatherObj, weatherState}) => {
     const [city, setCity] = useState('');
     return (
         <div className="SearchContainer">
@@ -13,10 +15,11 @@ const Search: React.FC<WeatherProps> = ({weatherObj}) => {
                 value={city}
                 onChange={(event) => setCity(event.target.value)}
                 type="text"
-                placeholder="Enter City and State">
+                placeholder="Enter City">
             </input>
-            <button onClick={() => weatherObj.getWeather(city)} className="SearchButton">Get weather
+            <button onClick={() => weatherObj.getWeather(city)} className="SearchButton">Find out!
             </button>
+            {<div className="errorMessage">{weatherObj.errorMessage}</div>}
         </div>
     );
 };
