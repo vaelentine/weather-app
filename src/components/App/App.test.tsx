290cdbebc0ca71669b2e5da0f2ba33563f7abcf5
vaelentine <magdalenegeise@gmail.com>
2021-08-03 11:09:48 -0700

    update app tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -5,17 +5,17 @@ import App from './App';
 describe('App', () => {
   it('should render a search container', () => {
     render(<App />);
-    const search_container = document.querySelector('#SearchContainer')
+    const search_container = document.querySelector('.SearchContainer')
     expect(search_container).not.toBeNull();
   })
   it('should render a weather container', () => {
     render(<App />);
-    const weather_container = document.querySelector('#WeatherContainer')
+    const weather_container = document.querySelector('.weatherContainer')
     expect(weather_container).not.toBeNull();
   })
   it('should render a footer', () => {
     render(<App />);
-    const footer_component = document.querySelector('#Footer')
+    const footer_component = document.querySelector('.Footer')
     expect(footer_component).not.toBeNull();
   })
 })
