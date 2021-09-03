75d634eda97085a2b30a20895e44cee87d0511fe
vaelentine <magdalenegeise@gmail.com>
2021-09-03 00:18:26 -0700

    refactor search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import OpenWeatherHandler from '../../services/OpenWeatherHandler'
+import React, { useState } from 'react'
 import './Search.css'
+import getWeather from 'api/getWeather'
 
-interface WeatherProps {
-    weatherObj: OpenWeatherHandler;
-    weatherState: any;
+const Search = () => {
+  const [city, setCity] = useState('')
+  return (
+    <div className="Search">
+      <input
+      placeholder="Enter city"
+      value={city}
+      onChange={(event) => setCity(event.target.value)}
+      type="text"
+      >
+      </input>
+      <button
+        className="SearchButton"
+        onClick={ () => getWeather(city)}
+      >
+        Find out!
+      </button>
+    </div>
+  )
 }
 
-const Search: React.FC<WeatherProps> = ({weatherObj, weatherState}) => {
-    const [city, setCity] = useState('');
-    return (
-        <div className="SearchContainer">
-            <input
-                value={city}
-                onChange={(event) => setCity(event.target.value)}
-                type="text"
-                placeholder="Enter City">
-            </input>
-            <button onClick={() => weatherObj.getWeather(city)} className="SearchButton">Find out!
-            </button>
-            {<div className="errorMessage">{weatherObj.errorMessage}</div>}
-        </div>
-    );
-};
-
-  export default Search;
+export default Search
