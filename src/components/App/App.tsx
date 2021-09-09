a9202ac8182339a112daf4f7afc86d7e5c49f064
vaelentine <magdalenegeise@gmail.com>
2021-09-09 14:26:19 -0700

    update app and snapshot

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
-import Search from '../Search/Search';
-import WeatherView from '../WeatherView/WeatherView';
-import OpenWeatherHandler from '../../services/OpenWeatherHandler'
-import Footer from '../Footer/Footer';
-import './App.css';
-
-const weatherObject = new OpenWeatherHandler();
-const missingApiKey: boolean = weatherObject.apiKey ===  undefined;
+import React, { useState } from 'react'
+import Search from '../Search/Search'
+import WeatherView from '../WeatherView/WeatherView'
+import Message from 'components/Message/Message'
+import Footer from '../Footer/Footer'
+import './App.css'
 
 function App() {
-  const [ weatherState, setWeatherState  ] = useState('')
-  weatherObject.assignState(setWeatherState);
+  const [weatherResponse, setWeatherResponse] = useState(null)
+  const handleResponse = (response:any|null) => {setWeatherResponse(response)}
+  const missingApiKey: boolean = process.env.REACT_APP_WEATHER_API_KEY === null
   return (
     <div className="appContainer">
       { missingApiKey && <div className="missingKey"> <p>Warning: It looks like your API key isn't configured. </p>
-      <p>Review the read me file for instructions to set one up.</p></div>}
+      <p>Review the readme file for instructions to set one up.</p></div>}
       <div className="App">
-        <div className="Header">
-        What's the weather in ...
-        </div>
-        <Search weatherObj={weatherObject} weatherState={weatherState}/>
-        <WeatherView weatherObj={weatherObject} weatherState={weatherState}/>
+        <div className="Header">What's the weather in ...</div>
+        <Search response={weatherResponse} onChange={handleResponse}/>
+        {weatherResponse && (
+          console.log(weatherResponse)
+          // weatherResponse.cod === 200
+          //   ? (<WeatherView weatherData={weatherResponse}/>)
+          //   : (
+          //   <Message messageData={weatherResponse}/>
+          //     )
+        )
+        }
+
         <Footer />
       </div>
     </div>
-
-  );
+  )
 }
 
-export default App;
+export default App
