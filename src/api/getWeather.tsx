6c1b284e729e4405ff6ec7189ec2ca1786d35557
vaelentine <magdalenegeise@gmail.com>
2021-09-19 14:18:52 -0700

    update api call

--- src/api/getWeather.tsx
+++ src/api/getWeather.tsx
@@ -3,21 +3,16 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname+'/.env' });
 // require('dotenv').config()
 
-const getWeather = async (city: string) => {
+async function getWeather(city: string) {
   try {
     const server = 'openweathermap.org/'
     const weatherEndpoint = 'data/2.5/weather'
     const keyParam = `appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-    const response = await axios.get(
-        `https://api.${server}
-              ${weatherEndpoint}
-              ?${keyParam}
-              &q=${city}
-              &units=imperial`
-    )
+    const request = `https://api.${server}${weatherEndpoint}?${keyParam}&q=${city}&units=imperial`
+    const response = await axios.get(request, {})
     return response
   } catch (error:any) {
-    return error.response.data
+    return error.response
   }
 }
 
